07ea755d9a693ff735415475ec70b0937ed0ca43
Dmitry Misharov <dmisharo@redhat.com>
2022-06-14 21:23:46 +0200

    - Python 3.8 is minimal version - Selenium 4.0 is minimal version - setuptools is replaced with hatch - setup.cfg is replaced with pyproject.toml - .pre-commit-config.yaml updated - flake8 config is moved to pre-commit - .isort.cfg is deleted

--- setup.py
+++
@@ -1,9 +0,0 @@
-import pkg_resources
-from setuptools import setup
-
-pkg_resources.require("setuptools>=40")
-
-if __name__ == "__main__":
-    setup(
-        setup_requires=["setuptools_scm"], use_scm_version=True, package_dir={"": "src"}
-    )
