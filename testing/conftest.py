f84cd59bcdb9bd4c529ca356bebb578958ebdbab
Iker Reyes <ireyes@redhat.com>
2022-07-27 16:37:54 +0200

    Use existing tests to test config

--- testing/conftest.py
+++ testing/conftest.py
@@ -3,13 +3,21 @@ import subprocess
 from urllib.request import urlopen
 
 import pytest
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from wait_for import wait_for
 
 BROWSER_IMAGE = "quay.io/redhatqe/selenium-standalone:latest"
 
+_CHROME_OPTIONS = ChromeOptions()
+_CHROME_OPTIONS.set_capability("acceptInsecureCerts", True)
+_CHROME_OPTIONS.add_argument("--disable-application-cache")
+
+
 CONFIGS = [
     pytest.param(
         {
+            "expected_browser_name": "firefox",
             "webdriver": "Remote",
             "proxy_url": "http://example.com:8080",
             "webdriver_options": {
@@ -24,6 +32,7 @@ CONFIGS = [
     ),
     pytest.param(
         {
+            "expected_browser_name": "chrome",
             "webdriver": "Remote",
             "proxy_url": "http://example.com:8080",
             "webdriver_options": {
@@ -39,6 +48,34 @@ CONFIGS = [
             },
         },
         id="remote-chrome",
+    ),
+    pytest.param(
+        {
+            "expected_browser_name": "firefox",
+            "webdriver": "Remote",
+            "proxy_url": "http://example.com:8080",
+            "webdriver_options": {
+                "command_executor": "http://127.0.0.1:4444",
+                "options": FirefoxOptions(),
+            },
+        },
+        id="remote-firefox-options",
+    ),
+    pytest.param(
+        {
+            "expected_browser_name": "chrome",
+            "webdriver": "Remote",
+            "proxy_url": "http://example.com:8080",
+            "webdriver_options": {
+                "command_executor": "http://127.0.0.1:4444",
+                "options": _CHROME_OPTIONS,
+                "desired_capabilities": {
+                    "browserName": "chrome",
+                    "goog:loggingPrefs": {"browser": "INFO", "performance": "ALL"},
+                },
+            },
+        },
+        id="remote-chrome-options",
     ),
 ]
 
@@ -68,10 +105,12 @@ def selenium_container():
 def test_data(selenium_container, request: pytest.FixtureRequest):
     from webdriver_kaifuku import BrowserManager, log
 
-    mgr = BrowserManager.from_conf(request.param)  # type: ignore
-    browser_name = request.param["webdriver_options"]["desired_capabilities"][  # type: ignore
-        "browserName"
-    ]
+    parameters = request.param  # type: ignore
+    browser_name = parameters.pop("expected_browser_name")
+
+    mgr = BrowserManager.from_conf(parameters)  # type: ignore
     log.warning(mgr)
     with contextlib.closing(mgr) as mgr:
         yield mgr, browser_name
+
+    parameters["expected_browser_name"] = browser_name
